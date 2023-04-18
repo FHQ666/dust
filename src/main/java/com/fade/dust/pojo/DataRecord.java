69fe50b676aa39c089117b60b388f45547278d4d
FHQ666 <2316331443@qq.com>
2023-04-18 23:55:22 +0800

    dust

--- src/main/java/com/fade/dust/pojo/DataRecord.java
+++ src/main/java/com/fade/dust/pojo/DataRecord.java
@@ -6,23 +6,47 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+//-------------------------------------------------------------
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
 @Data
-@TableName("data_record")
-public class DataRecord {
+@TableName("data")
+public class DataRecord implements Serializable {
 
-    @TableId(value = "id", type = IdType.AUTO)
+    @TableId(type = IdType.AUTO)
     private Integer id;
+    private Integer eCO2;
+    private Integer eCH20;
+    private Integer TVOC;
+    private Integer PM25;
+    private Integer PM10;
+    private Double Temperature;
+    private Double Humidity;
+    private Timestamp timestamp;
 
-    @TableField(value = "type")
-    private Integer type;
-
-    @TableField(value = "add_time")
-    private String addTime;
-
-    @TableField(value = "val")
-    private String val;
-
+    // 省略 getter 和 setter 方法
 }
+//----------------------------------------------------------
+
+//@TableName("data_record")
+//public class DataRecord {
+//
+//    @TableId(value = "id", type = IdType.AUTO)
+//    private Integer id;
+//
+//    @TableField(value = "type")
+//    private Integer type;
+//
+//    @TableField(value = "add_time")
+//    private String addTime;
+//
+//    @TableField(value = "val")
+//    private String val;
+//
+//}
 
 
--- src/main/java/com/fade/dust/test.java
+++
@@ -1,7 +0,0 @@
-package com.fade.dust;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello word!");
-    }
-}
